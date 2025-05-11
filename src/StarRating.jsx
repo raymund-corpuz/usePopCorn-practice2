c44a69598d4ba1412a3764f1e4bbf70e223da14d
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-05-11 08:13:03 +0800

    onHover

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -18,6 +18,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0); // State Variable
+  const [tempRating, setTempRating] = useState(0);
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
@@ -25,11 +26,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -41,9 +44,15 @@ const startStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={startStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={startStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
