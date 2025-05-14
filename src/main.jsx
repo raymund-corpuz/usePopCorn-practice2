2ebe985db989f85c0b7e3b7640d38df41668dad5
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-05-15 07:13:17 +0800

    Prop checking

--- src/main.jsx
+++ src/main.jsx
@@ -19,6 +19,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <StarRating
+      maxRating={"ssss"}
       message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
